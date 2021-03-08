acbeff39303dd57df4732fceb0b7f733811beac2
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-03-08 14:02:09 +0530

    olive : add support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,8 @@
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/aosp_olive.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_olive-user \
+    aosp_olive-userdebug \
+    aosp_olive-eng
